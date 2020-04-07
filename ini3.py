66017c4973ae486f5027f5068be004dbec340d7e
helio <helio@debit.com.br>
2020-04-07 20:19:22 -0300

    terceiro exercicio

---
+++ ini3.py
@@ -0,0 +1,9 @@
+s = 'C4LHKW3hWWa7uknhCtABsaXhxEMZgRaAy6hpR2xfJ0u2NBW7pqzOmky4vL7oAjx83d1d5VJfD0yr9GuxdPvmBrantaeC5LsoM2WglLe3IbAhNdIDu8nOn60pko5r43IgqmkXp6mtp7rpwqbRYPJ0y3dPGI43lBAs1S3pv7IbkurilensisxsZBiMp.'
+a = 84
+b = 89
+c = 168
+d = 177
+
+s = s[a:b+1] + ' ' + s[c:d+1]
+
+print(s)
